9541b6ac918541767b4753c5188a9db8c8f2837b
Chris Luhring <cluhring@gmail.com>
2015-10-09 13:14:25 -0600

    tweaked db seeding and api search

--- backend/app/models/trail.rb
+++ backend/app/models/trail.rb
@@ -7,7 +7,7 @@ class Trail < ActiveRecord::Base
   }
 
   scope :filter_by_lat_lng, -> (lat, lng) {
-    where("lat BETWEEN ? AND ?", lat - 0.1, lat + 0.1)
-    .where("lng BETWEEN ? AND ?", lng - 0.1, lng + 0.1)
+    where("lat BETWEEN ? AND ?", lat - 0.001, lat + 0.001)
+    .where("lng BETWEEN ? AND ?", lng - 0.001, lng + 0.001)
   }
 end
