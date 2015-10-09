2d8ef12a87a644df91397118e7cad2f1764979bf
Chris Luhring <cluhring@gmail.com>
2015-10-09 14:53:40 -0600

    leaflet routing added to trail map

--- frontend/app/components/trail-map.js
+++ frontend/app/components/trail-map.js
@@ -18,16 +18,43 @@ export default Ember.Component.extend({
 
     let lng = this.get('trail.lng');
 
-    map.setView([lat, lng], 15);
+    // map.setView([lat, lng], 15);
 
-    var points = { "type": "FeatureCollection",
-    "features": [
-      { "type": "Feature",
-      "geometry": {"type": "Point", "coordinates": [lng, lat]},
-      "properties": {"marker-symbol": "park", "marker-color": "#0C5CFE", "marker-size": "large"}
-       }]
-    };
+    // var points = { "type": "FeatureCollection",
+    // "features": [
+    //   { "type": "Feature",
+    //   "geometry": {"type": "Point", "coordinates": [lng, lat]},
+    //   "properties": {"marker-symbol": "park", "marker-color": "#0C5CFE", "marker-size": "large"}
+    //    }]
+    // };
 
-var myLayer = L.mapbox.featureLayer(points).addTo(map);
-}
+    map.locate({setView: false, maxZoom: 16});
+
+    function onLocationFound(e) {
+      var radius = e.accuracy / 2;
+      var current_lat = e.latlng.lat;
+      var current_long = e.latlng.lng;
+      // var ctrl = L.Routing.control({
+      //   waypoints: [
+      //     L.latLng (e.latlng.lat, e.latlng.lng),
+      //     L.latLng (lat, lng)
+      //   ],
+      //   routeWhileDragging: true,
+      //   draggableWaypoints: true,
+      //   autoRoute: true,
+      //   fitSelectedRoutes: 'smart',
+      //   geocoder: L.Control.Geocoder.nominatim()
+      // }).addTo(map);
+
+      L.marker(e.latlng).addTo(map)
+      .bindPopup("You are here").openPopup();
+
+      L.circle(e.latlng, radius).addTo(map);
+    }
+
+    map.on('locationfound', onLocationFound);
+
+    var myLayer = L.mapbox.featureLayer().addTo(map);
+    // var myLayer = L.mapbox.featureLayer(points).addTo(map);
+  }
 });
