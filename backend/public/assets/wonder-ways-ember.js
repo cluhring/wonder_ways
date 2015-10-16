17dd30857ec841b0223facf597eea50ae60df388
Chris Luhring <cluhring@gmail.com>
2015-10-16 13:36:22 -0600

    Asset compilation for deployment

--- backend/public/assets/wonder-ways-ember.js
+++ backend/public/assets/wonder-ways-ember.js
@@ -3445,7 +3445,7 @@ catch(err) {
 if (runningTests) {
   require("wonder-ways-ember/tests/test-helper");
 } else {
-  require("wonder-ways-ember/app")["default"].create({"name":"wonder-ways-ember","version":"0.0.0.f70d9814"});
+  require("wonder-ways-ember/app")["default"].create({"name":"wonder-ways-ember","version":"0.0.0.964bab5d"});
 }
 
 /* jshint ignore:end */
