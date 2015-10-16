a70f0a0d108de477f4f82f0a1467fc09b55c7ec5
Chris Luhring <cluhring@gmail.com>
2015-10-15 21:29:28 -0600

    where my jquery at?

--- frontend/app/components/trail-map.js
+++ frontend/app/components/trail-map.js
@@ -86,3 +86,15 @@ export default Ember.Component.extend({
     });
   }
 });
+
+$(document).ready(function(){
+  $("#direct").click(function(){
+    if($(this).text()==='Hide Directions'){
+      $('div.leaflet-routing-alternatives-container').hide();
+      $(this).text('Show Directions');
+    }else{
+      $('div.leaflet-routing-alternatives-container').show();
+      $(this).text('Hide Directions');
+    }
+  });
+});
