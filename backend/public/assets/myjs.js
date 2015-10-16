684181112626ea28baf26178469ff760198bfa84
Chris Luhring <cluhring@gmail.com>
2015-10-15 23:05:41 -0600

    modal added w/ jquery

--- backend/public/assets/myjs.js
+++ backend/public/assets/myjs.js
@@ -8,4 +8,12 @@ $(document).ready(function(){
       $(this).text('Hide Directions');
     }
   });
+  
+  $(".pretty-picture").each(function(){
+    $(this).click(function(){
+      $('#imagepreview').attr('src', $(this).attr('src')); // here asign the image to the modal when the user click the enlarge link
+      $('.modal-title').text($(this).attr('alt')); // here asign animal name to the modal when the user click the picture
+      $('#imagemodal').modal('toggle'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+    });
+  });
 });
