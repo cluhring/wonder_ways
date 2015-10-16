fe179c5b6befc04a1b206ab972984ff1de986b58
Chris Luhring <cluhring@gmail.com>
2015-10-16 10:54:39 -0600

    fixed back button from trail, need description to seed

--- frontend/public/assets/myjs.js
+++ frontend/public/assets/myjs.js
@@ -12,7 +12,19 @@ $(document).ready(function(){
   $(".pretty-picture").each(function(){
     $(this).click(function(){
       $('#imagepreview').attr('src', $(this).attr('src')); // here asign the image to the modal when the user click the enlarge link
-      $('.modal-title').text($(this).attr('alt')); // here asign animal name to the modal when the user click the picture
+      if ($(this).attr("title") !== "") {
+        $('.modal-title').html('<a href="https://en.wikipedia.org/w/index.php?title=' +
+          $(this).attr("alt").replace(/\s+/g, '_') +
+          '" class="modal-header" target="_blank" alt="' +
+          $(this).attr("alt") + '">' +
+          $(this).attr("title") + '</a>')
+        } else {
+        $('.modal-title').html('<a href="https://en.wikipedia.org/w/index.php?title=' +
+          $(this).attr("alt").replace(/\s+/g, '_') +
+          '" class="modal-header" target="_blank" alt="' +
+          $(this).attr("alt") + '">' +
+          $(this).attr("alt") + '</a>')
+        }; // here asign animal name + wikilink to the modal when the user click the picture
       $('#imagemodal').modal('toggle'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
     });
   });
